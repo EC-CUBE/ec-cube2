9f4cbb971d1eddd83f4560b12926f864dd78504c
scubism-tech-share <g_sol_product_develop@s-cubism.jp>
2015-03-26 12:52:29 +0900

    #40 帳票の文言を作成日から発行日に変更 (修正)

--- data/class/pages/admin/basis/LC_Page_Admin_Basis_Holiday.php
+++ data/class/pages/admin/basis/LC_Page_Admin_Basis_Holiday.php
@@ -165,7 +165,7 @@ class LC_Page_Admin_Basis_Holiday extends LC_Page_Admin_Ex
         switch ($mode) {
             case 'edit':
             case 'pre_edit':
-                $objFormParam->addParam('タイトル', 'title', SMTEXT_LEN, 'KVa', array('EXIST_CHECK','SPTAB_CHECK','MAX_LENGTH_CHECK'));
+                $objFormParam->addParam('タイトル', 'title', STEXT_LEN, 'KVa', array('EXIST_CHECK','SPTAB_CHECK','MAX_LENGTH_CHECK'));
                 $objFormParam->addParam('月', 'month', INT_LEN, 'n', array('SELECT_CHECK','SPTAB_CHECK','MAX_LENGTH_CHECK'));
                 $objFormParam->addParam('日', 'day', INT_LEN, 'n', array('SELECT_CHECK','SPTAB_CHECK','MAX_LENGTH_CHECK'));
                 $objFormParam->addParam('定休日ID', 'holiday_id', INT_LEN, 'n', array('NUM_CHECK', 'MAX_LENGTH_CHECK'));
