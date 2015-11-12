9c484d7a535fca923948ce5e8211c63d016118a0
Yangsin <yangsin_kim+github@lockon.co.jp>
2015-11-12 16:31:16 +0900

    version 表記のアップデート
    2.13.4 → 2.13.5

--- data/class/SC_Initial.php
+++ data/class/SC_Initial.php
@@ -35,7 +35,7 @@ class SC_Initial
     public function __construct()
     {
         /** EC-CUBEのバージョン */
-        define('ECCUBE_VERSION', '2.13.4');
+        define('ECCUBE_VERSION', '2.13.5');
     }
 
     /**
