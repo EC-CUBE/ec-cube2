f5129d9d3c351a46776d07c12da6f0327e5b72e2
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-08-25 15:49:04 +0900

    Composer support.

--- html/install/index.php
+++ html/install/index.php
@@ -20,6 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+require __DIR__.'/../../vendor/autoload.php';
 // ▼require.php 相当
 // rtrim は PHP バージョン依存対策
 $GLOBALS['_realdir'] = rtrim(realpath(rtrim(realpath(dirname(__FILE__)), '/\\') . '/../'), '/\\') . '/';
