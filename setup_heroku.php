05f23a74b874fb77788f318653c10dea51e5d3c2
のぶ <nobuhiko.kimoto@gmail.com>
2018-07-28 14:47:20 +0900

    引数不足を修正

--- setup_heroku.php
+++ setup_heroku.php
@@ -7,4 +7,4 @@ putenv("DBPASS=".getenv('DB_PASSWORD'));
 putenv("HTTP_URL=http://".getenv('HEROKU_APP_NAME').".herokuapp.com");
 putenv("HTTPS_URL=http://".getenv('HEROKU_APP_NAME').".herokuapp.com");
 
-exec("sh ./eccube_install.sh");
+exec("sh ./eccube_install.sh postgres");
