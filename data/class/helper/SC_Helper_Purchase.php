893daf4047f0057c913c02dc10e7a400a6b19594
SHUTTA, Takafumi <>
2019-03-03 23:46:42 +0900

    Fix issue of EC-CUBE#245
    mtb_delivery_dateの内容と合うようにコメント部分を修正。
    refs EC-CUBE#245

--- data/class/helper/SC_Helper_Purchase.php
+++ data/class/helper/SC_Helper_Purchase.php
@@ -588,19 +588,19 @@ class SC_Helper_Purchase
             case '3':
                 $start_day = 5;
                 break;
-                //1週間以内
+                //1週間以降
             case '4':
                 $start_day = 8;
                 break;
-                //2週間以内
+                //2週間以降
             case '5':
                 $start_day = 15;
                 break;
-                //3週間以内
+                //3週間以降
             case '6':
                 $start_day = 22;
                 break;
-                //1ヶ月以内
+                //1ヶ月以降
             case '7':
                 $start_day = 32;
                 break;
