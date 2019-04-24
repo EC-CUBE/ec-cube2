8859a591515dcf5d230c67e024c5cd54d8cbd1ef
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-04-24 13:22:17 +0900

    tests 以下の classmap を生成するよう修正

--- tests/require.php
+++ tests/require.php
@@ -1,6 +1,22 @@
 <?php
+$loader = require __DIR__.'/../data/vendor/autoload.php';
+
 if (!class_exists('PHPUnit_Framework_TestCase')) {
     class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
 }
 require_once __DIR__."/../html/require.php";
-require_once __DIR__."/class/Common_TestCase.php";
+
+$classMap = function ($dir) {
+    $map = [];
+    $directoryIterator = new RecursiveDirectoryIterator($dir);
+    $iterator = new RecursiveIteratorIterator($directoryIterator);
+    foreach ($iterator as $fileinfo) {
+        /** @var SplFileInfo $fileinfo */
+        if ($fileinfo->isFile()) {
+            $map[str_replace('.'.$fileinfo->getExtension(), '', $fileinfo->getFilename())] = $fileinfo->getPathname();
+        }
+    }
+    return $map;
+};
+$loader->addClassMap($classMap(__DIR__.'/class'));
+return $loader;
