342f088bebd76bc0fd7f34d1878c0a4865858ab4
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-05-16 15:09:08 +0900

    Add NUM_CHECK

---
+++ tests/class/SC_CheckError/SC_CheckError_NUM_CHECKTest.php
@@ -0,0 +1,75 @@
+<?php
+
+class SC_CheckError_NUM_CHECKTest extends SC_CheckError_AbstractTestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->target_func = 'NUM_CHECK';
+    }
+
+    public function testNUM_CHECK()
+    {
+        $this->arrForm = [self::FORM_NAME => 'a'];
+        $this->expected = '※ NUM_CHECKは数字で入力してください。<br />';
+
+        $this->scenario();
+        $this->verify();
+    }
+
+
+    public function testNUM_CHECKWithNoError()
+    {
+        $this->arrForm = [self::FORM_NAME => 5];
+        $this->expected = '';
+
+        $this->scenario();
+        $this->verify();
+    }
+
+    public function testNUM_CHECKWithEmpty()
+    {
+        $this->arrForm = [self::FORM_NAME => ''];
+        $this->expected = '';
+
+        $this->scenario();
+        $this->verify();
+    }
+
+    public function testNUM_CHECKWithNull()
+    {
+        $this->arrForm = [self::FORM_NAME => null];
+        $this->expected = '';
+
+        $this->scenario();
+        $this->verify();
+    }
+
+    public function testNUM_CHECKWithString()
+    {
+        $this->arrForm = [self::FORM_NAME => '5'];
+        $this->expected = '';
+
+        $this->scenario();
+        $this->verify();
+    }
+
+    public function testNUM_CHECKWithFloat()
+    {
+        $this->arrForm = [self::FORM_NAME => 1.1];
+        $this->expected = '※ NUM_CHECKは数字で入力してください。<br />';
+
+        $this->scenario();
+        $this->verify();
+    }
+
+    public function testNUM_CHECKWithIntmax()
+    {
+        $this->arrForm = [self::FORM_NAME => PHP_INT_MAX];
+        $this->expected = '';
+
+        $this->scenario();
+        $this->verify();
+    }
+}
+
