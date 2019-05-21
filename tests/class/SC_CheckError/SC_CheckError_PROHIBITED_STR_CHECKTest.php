508f6f6844a92b4bf3111da72b5e3aae489c474d
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-05-21 17:38:31 +0900

    Add PROHIBITED_STR_CHECK

---
+++ tests/class/SC_CheckError/SC_CheckError_PROHIBITED_STR_CHECKTest.php
@@ -0,0 +1,67 @@
+<?php
+
+class SC_CheckError_PROHIBITED_STR_CHECKTest extends SC_CheckError_AbstractTestCase
+{
+    /** @var Faker\Generator */
+    protected $faker;
+
+    /** @var array */
+    protected $denyStrings;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->target_func = 'PROHIBITED_STR_CHECK';
+        $this->faker = Faker\Factory::create('ja_JP');
+        $this->denyStrings = ['aaa', 'bbb', 'ccc'];
+    }
+
+    public function testPROHIBITED_STR_CHECK()
+    {
+        $this->arrForm = [
+            self::FORM_NAME => 'ddd'
+        ];
+        $this->expected = '';
+
+        $this->scenario();
+        $this->verify();
+    }
+
+
+    public function testPROHIBITED_STR_CHECKWithInvalid()
+    {
+        $this->arrForm = [self::FORM_NAME => 'aaaaa'];
+        $this->expected = '※ PROHIBITED_STR_CHECKは入力できません。<br />';
+
+        $this->scenario();
+        $this->verify();
+    }
+
+    public function testPROHIBITED_STR_CHECKWithEmpty()
+    {
+        $this->arrForm = [self::FORM_NAME => ''];
+        $this->expected = '';
+
+        $this->scenario();
+        $this->verify();
+    }
+
+    public function testPROHIBITED_STR_CHECKWithNull()
+    {
+        $this->arrForm = [self::FORM_NAME => null];
+        $this->expected = '';
+
+        $this->scenario();
+        $this->verify();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenario()
+    {
+        $this->objErr = new SC_CheckError_Ex($this->arrForm);
+        $this->objErr->doFunc([$this->target_func, self::FORM_NAME, $this->denyStrings], [$this->target_func]);
+        $this->objErr->doFunc(['dummy', self::FORM_NAME, $this->denyStrings], [$this->target_func]);
+    }
+}
