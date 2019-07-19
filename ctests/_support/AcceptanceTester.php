b8d2692f695200df50d2f6ef7427b6616dc93fbd
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-07-19 15:57:20 +0900

    売上集計のテストを追加
    - CSVダウンロードのチェック追加

--- ctests/_support/AcceptanceTester.php
+++ ctests/_support/AcceptanceTester.php
@@ -23,4 +23,37 @@ class AcceptanceTester extends \Codeception\Actor
    /**
     * Define custom actions here
     */
+
+    /**
+     * @param string|$fileNameRegex ファイル名のパターン(CI環境で同時実行したときに区別するため)
+     * @param int $retryCount リトライカウント数
+     * @param string $downloadDir ダウンロードディレクトリ
+     *
+     * @return string|bool ファイルパス. ファイルが見つからない場合 false
+     */
+    public function getLastDownloadFile($fileNameRegex, $retryCount = 3, $downloadDir = '/tmp')
+    {
+        $files = scandir($downloadDir);
+        $files = array_map(function ($fileName) use ($downloadDir) {
+            return $downloadDir.'/'.$fileName;
+        }, $files);
+        $files = array_filter($files, function ($f) use ($fileNameRegex) {
+            return is_file($f) && preg_match($fileNameRegex, basename($f));
+        });
+
+        usort($files, function ($l, $r) {
+            return filemtime($l) - filemtime($r);
+        });
+
+        if (empty($files)) {
+            if ($retryCount > 0) {
+                $this->wait(3);
+
+                return $this->getLastDownloadFile($fileNameRegex, $retryCount - 1, $downloadDir);
+            }
+            return false;
+        }
+
+        return end($files);
+    }
 }
