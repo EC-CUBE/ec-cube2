c57467660413e47e69a63478e5470b32f80c6fc8
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-08-26 17:09:23 +0900

    テストケース追加

--- tests/class/helper/SC_Helper_TaxRule/SC_Helper_TaxRule_getTaxRule_OptionProductTaxRuleTest.php
+++ tests/class/helper/SC_Helper_TaxRule/SC_Helper_TaxRule_getTaxRule_OptionProductTaxRuleTest.php
@@ -130,4 +130,38 @@ class SC_Helper_TaxRule_getTaxRule_OptionProductTaxRuleTest extends SC_Helper_Ta
 
         $this->verify();
     }
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     *
+     * 基本税率と同じ税率を設定すると商品別税率を削除する
+     * @see https://github.com/EC-CUBE/eccube-2_13/issues/304
+     */
+    public function testDeletedProductTaxRate()
+    {
+        $arrDefaultTaxRule = SC_Helper_TaxRule_Ex::getTaxRule();
+        // 基本税率と同じ税率を商品ID: 1000 に設定する
+        $this->objTaxRule->setTaxRuleForProduct($arrDefaultTaxRule['tax_rate'], 1000);
+        $return = $this->objTaxRule->getTaxRule(1000, 0, 0, 0, self::OPTION_PRODUCT_TAX_RULE_ENABLE);
+
+        $this->actual = array(
+            'apply_date' => $return['apply_date'],
+            'tax_rate' => $return['tax_rate'],
+            'product_id' => $return['product_id'],
+            'product_class_id' => $return['product_class_id'],
+            'del_flg' => $return['del_flg']
+        );
+
+        $this->expected = array(
+            'apply_date' => $arrDefaultTaxRule['apply_date'],
+            'tax_rate' => $arrDefaultTaxRule['tax_rate'],
+            'product_id' => '0',
+            'product_class_id' => '0',
+            'del_flg' => '0'
+        );
+
+
+        $this->verify('基本税率が取得できるはず');
+    }
 }
