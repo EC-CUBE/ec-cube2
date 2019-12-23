eac1498481b73c96ab3a015b6778901d798c92f8
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-12-23 09:58:44 +0900

    テストケース追加

---
+++ tests/class/fixtures/plugin/FixturePlugin/FixturePlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+class FixturePlugin extends SC_Plugin_Base
+{
+    public function loadClassFileChange(&$classname, &$classpath) {
+        var_dump($classpath);
+
+        if ($classname === "SC_Customer_Ex") {
+            $classpath = "FixturePlugin/Fixture_SC_Customer.php";
+            $classname = "Fixture_SC_Customer";
+        }
+    }
+}
