dcc550d68c895155efa5add95935f0d3d33cb9e2
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-12-23 13:58:33 +0900

    Remove

--- tests/class/fixtures/plugin/FixturePlugin/FixturePlugin.php
+++ tests/class/fixtures/plugin/FixturePlugin/FixturePlugin.php
@@ -3,8 +3,6 @@
 class FixturePlugin extends SC_Plugin_Base
 {
     public function loadClassFileChange(&$classname, &$classpath) {
-        var_dump($classpath);
-
         if ($classname === "SC_Customer_Ex") {
             $classpath = "FixturePlugin/Fixture_SC_Customer.php";
             $classname = "Fixture_SC_Customer";
