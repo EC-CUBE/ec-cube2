bdd295bad7e96a248dbf96526243883e955bc1d7
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2020-01-20 18:36:00 +0900

    prefilterTransform の E2Eテストを追加

---
+++ tests/class/fixtures/plugin/PrefilterTransformPlugin/plugin_info.php
@@ -0,0 +1,13 @@
+<?php
+class plugin_info {
+    static $PLUGIN_CODE        = 'PrefilterTransformPlugin';
+    static $PLUGIN_NAME        = 'PrefilterTransformPlugin';
+    static $CLASS_NAME         = 'PrefilterTransformPlugin';
+    static $PLUGIN_VERSION     = '0.0.0';
+    static $COMPLIANT_VERSION  = '2.17';
+    static $AUTHOR             = 'dummy';
+    static $DESCRIPTION        = 'dummy';
+    static $HOOK_POINTS        = [
+        ['prefilterTransform', 'prefilterTransform']
+    ];
+}
