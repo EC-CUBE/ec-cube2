7b99d7ea8d663e8811a60c9b73140b1a9269bf5b
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2020-03-14 16:43:51 +0900

    delete duplicate

--- data/class/pages/admin/products/LC_Page_Admin_Products_UploadCSVCategory.php
+++ data/class/pages/admin/products/LC_Page_Admin_Products_UploadCSVCategory.php
@@ -30,7 +30,7 @@ require_once CLASS_EX_REALDIR . 'page_extends/admin/LC_Page_Admin_Ex.php';
  *
  * @package Page
  * @author EC-CUBE CO.,LTD.
- * @version $$Id$$
+ * @version $Id$
  */
 class LC_Page_Admin_Products_UploadCSVCategory extends LC_Page_Admin_Ex
 {
