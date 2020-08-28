37a77d42c91d7a69eb9e588195a6e703eca252b3
のぶ <nobuhiko.kimoto@gmail.com>
2020-08-28 14:22:32 +0900

    テストから画像確認部分を削除

--- ctests/acceptance/admin/total/AdminTotalCept.php
+++ ctests/acceptance/admin/total/AdminTotalCept.php
@@ -30,8 +30,6 @@ $I->seeInField('select[name=search_endday]', date('j'));
 $I->amGoingTo('売上集計>期間別集計>月度集計');
 $I->click('月度で集計する');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-term']);
 
@@ -39,40 +37,30 @@ $I->amGoingTo('売上集計>期間別集計>期間集計');
 $I->selectOption('select[name=search_startyear]', date('Y', strtotime('-1 year')));
 $I->click('期間で集計する');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-term']);
 
 $I->amGoingTo('売上集計>期間別集計>期間集計>月別');
 $I->click('月別');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-term']);
 
 $I->amGoingTo('売上集計>期間別集計>期間集計>年別');
 $I->click('年別');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-term']);
 
 $I->amGoingTo('売上集計>期間別集計>期間集計>曜日別');
 $I->click('曜日別');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-term']);
 
 $I->amGoingTo('売上集計>期間別集計>期間集計>時間別');
 $I->click('時間別');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-term']);
 
@@ -98,8 +86,6 @@ $I->seeInField('select[name=search_endday]', date('j'));
 $I->amGoingTo('売上集計>商品別集計>月度集計');
 $I->click('月度で集計する');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-products']);
 
@@ -107,24 +93,18 @@ $I->amGoingTo('売上集計>商品集計>期間集計');
 $I->selectOption('select[name=search_startyear]', date('Y', strtotime('-1 year')));
 $I->click('期間で集計する');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-products']);
 
 $I->amGoingTo('売上集計>商品集計>期間集計>会員');
 $I->click('会員');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-products']);
 
 $I->amGoingTo('売上集計>商品集計>期間集計>非会員');
 $I->click('非会員');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-products']);
 
@@ -150,8 +130,6 @@ $I->seeInField('select[name=search_endday]', date('j'));
 $I->amGoingTo('売上集計>年代別集計>月度集計');
 $I->click('月度で集計する');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-age']);
 
@@ -159,24 +137,18 @@ $I->amGoingTo('売上集計>年代別集計>期間集計');
 $I->selectOption('select[name=search_startyear]', date('Y', strtotime('-1 year')));
 $I->click('期間で集計する');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-age']);
 
 $I->amGoingTo('売上集計>年代別集計>期間集計>会員');
 $I->click('会員');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-age']);
 
 $I->amGoingTo('売上集計>年代別集計>期間集計>非会員');
 $I->click('非会員');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-age']);
 
@@ -202,8 +174,6 @@ $I->seeInField('select[name=search_endday]', date('j'));
 $I->amGoingTo('売上集計>職業別集計>月度集計');
 $I->click('月度で集計する');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-job']);
 
@@ -211,8 +181,6 @@ $I->amGoingTo('売上集計>職業別集計>期間集計');
 $I->selectOption('select[name=search_startyear]', date('Y', strtotime('-1 year')));
 $I->click('期間で集計する');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-job']);
 
@@ -238,8 +206,6 @@ $I->seeInField('select[name=search_endday]', date('j'));
 $I->amGoingTo('売上集計>会員別集計>月度集計');
 $I->click('月度で集計する');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-member']);
 
@@ -247,8 +213,6 @@ $I->amGoingTo('売上集計会員別集計>期間集計');
 $I->selectOption('select[name=search_startyear]', date('Y', strtotime('-1 year')));
 $I->click('期間で集計する');
 
-$I->expect('グラフの表示を確認する');
-$I->seeElement(['css' => '#graph-image > img']);
 $I->expect('表の表示を確認する');
 $I->seeElement(['id' => 'total-member']);
 
