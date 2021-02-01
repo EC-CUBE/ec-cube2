7668d01e5ed9c10adf3bdb5da9d989e0f8719536
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2021-02-01 16:55:30 +0900

    error_reporting(E_ALL) に設定

--- data/class/SC_Initial.php
+++ data/class/SC_Initial.php
@@ -106,10 +106,6 @@ class SC_Initial
     public function setErrorReporting()
     {
         error_reporting(E_ALL);
-        // PHP 5.3.0対応
-        if (error_reporting() > 6143) {
-            error_reporting(E_ALL);
-        }
     }
 
     /**
