ac5cb1c39009cc5b8678104aaeb9d897b3706eb1
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2022-02-28 18:00:14 +0900

    [WIP]ルーティングの実装

--- html/index.php
+++ html/index.php
@@ -20,9 +20,31 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-require_once './require.php';
-require_once CLASS_EX_REALDIR . 'page_extends/LC_Page_Index_Ex.php';
+require __DIR__.'/require.php';
+$route = require HTML_REALDIR.HTML2DATA_DIR.'routing/Route.php';
+$dispatcher = FastRoute\simpleDispatcher($routes);
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
 
-$objPage = new LC_Page_Index_Ex();
-$objPage->init();
-$objPage->process();
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        header('HTTP', true, 404);
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        header('HTTP', true, 405);
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        echo $handler($vars);
+        break;
+}
