77d459e3a5a2dab1097ef83c2d25ff4b68cea5a7
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2022-04-05 16:57:40 +0900

    data/class_extends/page_extends/frontparts/bloc/ 以下の *_Ex ファイルを削除

--- html/frontparts/bloc/calendar.php
+++ html/frontparts/bloc/calendar.php
@@ -22,7 +22,6 @@
  */
 
 require_once realpath(dirname(__FILE__)) . '/../../require.php';
-require_once CLASS_EX_REALDIR . 'page_extends/frontparts/bloc/LC_Page_FrontParts_Bloc_Calendar_Ex.php';
 
 $objPage = new LC_Page_FrontParts_Bloc_Calendar_Ex();
 $objPage->blocItems = $params['items'];
