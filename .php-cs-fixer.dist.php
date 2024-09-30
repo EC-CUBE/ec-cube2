94d493cd94b96a0b337b11a6c09da17869c282ae
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2024-10-01 01:49:53 +0900

    php-cs-fixer のルールを整理
    - コメント追記
    - `single_quote => true` に変更(4系にあわせる)
    - `concat_space => ['spacing' => 'none']` に変更(4系にあわせる)
    - `array_syntax => true` に変更(4系にあわせる)
    - `phpdoc_scalar => true` に変更(phpdoc の int や bool を統一したい)
    - `data/smarty_extends` 及び `tests` にも適用

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,28 +8,30 @@ $rules = [
     '@Symfony' => true,
     '@Symfony:risky' => true,
     // '@PHP83Migration' => true,
-    'psr_autoloading' => false,
-    'single_quote' => false,
-    'is_null' => false,
-    'concat_space' => ['spacing' => 'one'],
-    'native_constant_invocation' => false,
-    'array_syntax' => false,
-    'phpdoc_align' => false,
-    'phpdoc_summary' => false,
-    'phpdoc_scalar' => false,
-    'phpdoc_annotation_without_dot' => false,
-    'no_superfluous_phpdoc_tags' => false,
-    'increment_style' => false,
-    'yoda_style' => false,
-    'ereg_to_preg' => true,
-    'string_length_to_empty' => false,
-    'ternary_to_elvis_operator' => false,
+
+    // @Symfony のうち、以下のルールを無効化
+    'phpdoc_align' => false, // phpdoc の内容が削除されてしまう場合がある
+    'phpdoc_summary' => false, // 日本語なので不要
+    'phpdoc_annotation_without_dot' => false, // 日本語なので不要
+    'no_superfluous_phpdoc_tags' => false, // 副作用があるため
+    'increment_style' => false, // 強制しなくて良い
+    'yoda_style' => false, // 強制しなくて良い
+    'trailing_comma_in_multiline' => false, // `, ]` となるケースを防ぐため
+
+    // @Symfony:risky のうち、以下のルールを無効化
+    'psr_autoloading' => false, // PSR-4 に準拠していないため
+    'is_null' => false, // 副作用があるため
+    'native_constant_invocation' => false, // namespace を使用していないため不要
+    'string_length_to_empty' => false, // 副作用があるため
+    'ternary_to_elvis_operator' => false, // 副作用があるため
 ];
 
 $finder = \PhpCsFixer\Finder::create()
     ->in(__DIR__.'/data/class')
     ->in(__DIR__.'/data/class_extends')
     ->in(__DIR__.'/data/module')
+    ->in(__DIR__.'/data/smarty_extends')
+    ->in(__DIR__.'/tests')
     ->name('*.php')
 ;
 $config = new \PhpCsFixer\Config();
