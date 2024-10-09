8fc38834696be0b08b25250ff1a2644982227f7a
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2024-10-10 01:52:52 +0900

    Fix risky

--- tests/class/helper/SC_Helper_News/SC_Helper_News_saveNewsTest.php
+++ tests/class/helper/SC_Helper_News/SC_Helper_News_saveNewsTest.php
@@ -20,36 +20,38 @@ class SC_Helper_News_saveNewsTest extends SC_Helper_News_TestBase
 
     public function testSaveNewsTestNewsIdが空の場合新規登録される()
     {
-        if (DB_TYPE != 'pgsql') { // postgresqlだとどうしてもDBエラーになるのでとりいそぎ回避
-            $objQuery = &SC_Query_Ex::getSingletonInstance();
-            $this->setUpNews();
-
-            $sqlval = [
-            'news_title' => 'ニュース情報05',
-            'creator_id' => '1',
-            'del_flg' => '0',
-            ];
-
-            $this->expected['count'] = '5';
-            $this->expected['content'] = [
-            'news_title' => 'ニュース情報05',
-            'creator_id' => '1',
-            'del_flg' => '0',
-            ];
-
-            // $sqlval['news_id'] = $objQuery->setVal('dtb_news_news_id', 5);
-            $ret_id = $this->objNews->saveNews($sqlval);
-
-            $this->actual['count'] = $objQuery->count('dtb_news');
-            $result = $objQuery->select(
-                'news_title, creator_id, del_flg',
-                'dtb_news',
-                'news_id = ?',
-                [$ret_id]);
-            $this->actual['content'] = $result[0];
-
-            $this->verify();
+        if (DB_TYPE == 'pgsql') {
+            $this->markTestSkipped('postgresqlだとどうしてもDBエラーになるのでスキップ');
         }
+
+        $objQuery = &SC_Query_Ex::getSingletonInstance();
+        $this->setUpNews();
+
+        $sqlval = [
+            'news_title' => 'ニュース情報05',
+            'creator_id' => '1',
+            'del_flg' => '0',
+        ];
+
+        $this->expected['count'] = '5';
+        $this->expected['content'] = [
+            'news_title' => 'ニュース情報05',
+            'creator_id' => '1',
+            'del_flg' => '0',
+        ];
+
+        // $sqlval['news_id'] = $objQuery->setVal('dtb_news_news_id', 5);
+        $ret_id = $this->objNews->saveNews($sqlval);
+
+        $this->actual['count'] = $objQuery->count('dtb_news');
+        $result = $objQuery->select(
+            'news_title, creator_id, del_flg',
+            'dtb_news',
+            'news_id = ?',
+            [$ret_id]);
+        $this->actual['content'] = $result[0];
+
+        $this->verify();
     }
 
     public function testSaveNewsTestNewsIdが存在する場合対象のニュースが更新される()
