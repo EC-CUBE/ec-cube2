b67346109d0504cd93adbc476f41e0c7a1845925
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2024-12-17 10:35:18 +0900

    コメント削除

--- tests/class/modifier/Modifier_ScriptEscapeTest.php
+++ tests/class/modifier/Modifier_ScriptEscapeTest.php
@@ -2,9 +2,6 @@
 
 /**
  * (省略。アノテーションを認識されるのに必要なようなので記述している。)
- *
- * PHP 8.1 でグローバル変数が消失する不具合を回避するため、下で `backupGlobals` を指定している。本質的には PHPUnit が PHP8 に対応していないのが原因と考えられる。
- *
  */
 class Modifier_ScriptEscapeTest extends PHPUnit_Framework_TestCase
 {
