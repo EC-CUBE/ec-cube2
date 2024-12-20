5913be6f320ea1c8c62c337c41cbb6a83f5ff4a1
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2024-12-20 15:22:34 +0900

    endpoints.csv に記載されたURLがすべて表示されることを確認するテストを追加

---
+++ e2e-tests/utils/EndpointReader.ts
@@ -0,0 +1,24 @@
+import * as fs from 'fs';
+import * as path from "path";
+
+export class EndpointReader {
+  endpoints: Array<string>;
+
+  constructor() {
+    this.read();
+  }
+
+  read() {
+    const excludes = fs.readFileSync(path.join(__dirname, '..', 'endpoints', 'exclude_endpoints.csv'), 'utf8').split('\n');
+    this.endpoints = fs.readFileSync(path.join(__dirname, '..', 'endpoints', 'endpoints.csv'), 'utf8')
+      .split('\n')
+      .filter(line => !excludes.includes(line))
+      .map(line => line.replace(/index.*/, ''));
+
+    return this.endpoints;
+  }
+
+  filter(prefix: string) {
+    return this.endpoints.filter(line => line.match(new RegExp(`^${prefix}`)));
+  }
+}
