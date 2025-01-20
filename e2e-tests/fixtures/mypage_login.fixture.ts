8c474c40ace8b341bf55e33b1b8d8fdbdc55c2c6
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-01-20 13:43:53 +0900

    ポイントを加算するよう修正

--- e2e-tests/fixtures/mypage_login.fixture.ts
+++ e2e-tests/fixtures/mypage_login.fixture.ts
@@ -1,9 +1,11 @@
 import { test as base, expect } from "@playwright/test";
 import { MypageLoginPage } from "../pages/mypage/login.page";
 import { EntryPage } from "../pages/entry/entry.page";
+import { AdminLoginPage } from "../pages/admin/login.page";
 import { Mode, ContextType } from '../utils/ZapClient';
-import { ECCUBE_DEFAULT_USER, ECCUBE_DEFAULT_PASS } from "../config/default.config";
 import PlaywrightConfig from '../../playwright.config';
+import { ECCUBE_ADMIN_USER, ECCUBE_ADMIN_PASS, ADMIN_DIR } from "../config/default.config";
+import { faker } from '@faker-js/faker/locale/ja';
 import { FakerUtils } from "../utils/FakerUtils";
 
 type MypageLoginFixtures = {
@@ -24,6 +26,20 @@ export const test = base.extend<MypageLoginFixtures>({
       await entryPage.register();
       await loginPage.goto();
       await loginPage.logout();
+
+      // 購入フローのテストでポイント利用するため、ポイントを加算する
+      const adminLoginPage = new AdminLoginPage(page);
+      await page.goto(`/${ ADMIN_DIR }`);
+      await adminLoginPage.login(ECCUBE_ADMIN_USER, ECCUBE_ADMIN_PASS);
+      await page.goto(`/${ ADMIN_DIR }/customer/index.php`);
+      await page.locator('input[name=search_email]').fill(email);
+      await page.getByRole('link', { name: 'この条件で検索する' }).click();
+      await page.getByRole('link', { name: '編集' }).click();
+      await page.getByRole('row', { name: '所持ポイント' }).getByRole('textbox').fill(String(faker.number.int({ min: 0, max: 999999 })));
+      await page.getByRole('link', { name: '確認ページへ' }).click();
+      await page.getByRole('link', { name: 'この内容で登録する' }).click();
+
+      await page.goto(`/`);
       await loginPage.login();
     } else {
       const zapClient = loginPage.getZapClient();
