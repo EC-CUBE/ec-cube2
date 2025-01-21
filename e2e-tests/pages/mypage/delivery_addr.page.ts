c7a0d00250e046608700b75cb43ee984e047210e
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-01-21 11:51:48 +0900

    コミット漏れ

---
+++ e2e-tests/pages/mypage/delivery_addr.page.ts
@@ -0,0 +1,24 @@
+import { Locator, Page } from '@playwright/test';
+import { PersonalInputPage } from '../personal_input.page';
+
+export class MypageDeliveryAddrPage {
+  readonly personalInputPage: PersonalInputPage;
+  readonly registerButton: Locator;
+
+  constructor(page: Page) {
+    this.personalInputPage = new PersonalInputPage(page);
+    this.registerButton = page.locator('[alt=登録する]');
+  }
+
+  async fill() {
+    await this.personalInputPage.fillName();
+    await this.personalInputPage.fillCompany();
+    await this.personalInputPage.fillAddress();
+    await this.personalInputPage.fillTel();
+    await this.personalInputPage.fillFax();
+  }
+
+  async register() {
+    await this.registerButton.click();
+  }
+}
