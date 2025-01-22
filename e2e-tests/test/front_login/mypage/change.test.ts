a3cd81e1527dc0581d6a4c40623152f35ab11cfc
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-01-22 12:15:50 +0900

    会員登録内容変更のテスト

--- e2e-tests/test/front_login/mypage/change.test.ts
+++ e2e-tests/test/front_login/mypage/change.test.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '../../../fixtures/mypage_login.fixture';
+import { EntryPage } from '../../../pages/entry/entry.page';
 import PlaywrightConfig from '../../../../playwright.config';
 
 const url = `${ PlaywrightConfig.use?.baseURL ?? '' }/mypage/change.php`;
@@ -6,6 +7,14 @@ test.describe.serial('会員登録内容変更画面のテストをします', (
   test('会員登録内容変更画面のテストをします', async ( { mypageLoginPage, page }) => {
     await page.goto(url);
     await expect(page).toHaveTitle(/会員登録内容変更/);
+    const entryPage = new EntryPage(page);
+    await entryPage.fill();
+    const name01 = await entryPage.personalInputPage.name01.inputValue();
+    const name02 = await entryPage.personalInputPage.name02.inputValue();
+    await entryPage.confirm();
+    await page.locator('id=complete').click();
+    await expect(page).toHaveTitle(/会員登録内容変更\(完了ページ\)/);
+    await expect(page.locator('id=header_login_area'), '名前が変更されているのを確認').toContainText(`${name01} ${name02}`);
   });
 
   test('LC_Page_Mypage_Change_Ex クラスのテストをします @extends', async ( { page }) => {
