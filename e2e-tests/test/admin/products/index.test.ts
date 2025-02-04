e1aa575da8627ad7fbbc2785f724ad09bb772636
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-02-04 14:50:18 +0900

    商品編集のテストを追加

--- e2e-tests/test/admin/products/index.test.ts
+++ e2e-tests/test/admin/products/index.test.ts
@@ -9,4 +9,21 @@ test.describe('商品マスターのテストをします', () => {
     await page.getByRole('link', { name: 'この条件で検索する' }).click();
     await expect(page.locator('table.list').getByText(adminProductsProductPage.productName)).toBeVisible();
   });
+
+  test('商品編集のテストをします', async ({ page, adminProductsProductPage }) => {
+    await page.goto(url);
+    await page.getByRole('row', { name: '商品名' }).getByRole('textbox').nth(1).fill(adminProductsProductPage.productName);
+    await page.getByRole('link', { name: 'この条件で検索する' }).click();
+    await page.locator('table.list').getByRole('row').nth(2).getByRole('link', { name: '編集' }).click();
+    await expect(page.getByRole('row', { name: '商品名' }).locator('input[name=name]')).toHaveValue(adminProductsProductPage.productName);
+
+    await test.step('商品名を編集します', async () => {
+      await adminProductsProductPage.name.first().fill(`${adminProductsProductPage.productName}を編集`);
+      await adminProductsProductPage.gotoConfirm();
+      await adminProductsProductPage.register();
+      await expect(page.getByText('登録が完了致しました')).toBeVisible();
+      await page.getByRole('link', { name: '検索結果へ戻る' }).click();
+    });
+    await expect(page.locator('table.list').getByText(`${adminProductsProductPage.productName}を編集`)).toBeVisible();
+  });
 });
