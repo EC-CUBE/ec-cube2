6e9a3610c4d2aacc94f1b259bfc849856260b72b
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-02-04 13:50:48 +0900

    product.test.ts のリファクタリング

--- e2e-tests/test/admin/products/product.test.ts
+++ e2e-tests/test/admin/products/product.test.ts
@@ -1,17 +1,11 @@
-import { test, expect } from '../../../fixtures/admin/admin_login.fixture';
-import { AdminProductsProductPage } from '../../../pages/admin/products/product.page';
+import { test, expect } from '../../../fixtures/admin/register_product.fixture.ts';
 import { ADMIN_DIR } from '../../../config/default.config';
 
 const url = `${ADMIN_DIR}/products/product.php`;
 test.describe('商品登録画面のテストをします', () => {
-  test('商品登録画面のテストをします', async ({ page, adminLoginPage }) => {
-    const productPage = new AdminProductsProductPage(page);
-    await productPage.goto();
-    await productPage.fill();
-    await productPage.fillSubComments();
-    await productPage.fillRecommends();
-    await productPage.gotoConfirm();
-    await productPage.register();
-    await expect(page.getByText('登録が完了致しました')).toBeVisible();
+  test('商品登録画面のテストをします', async ({ page, adminProductsProductPage }) => {
+    await test.step('register_product.fixture で商品を登録します', async () => {
+      await expect(page.getByText('登録が完了致しました')).toBeVisible();
+    });
   });
 });
