eb12d5fc2633c503cfab5e656bc69138b8b62d69
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-03-19 18:59:05 +0900

    Apply php-cs-fixer

--- data/class_extends/page_extends/admin/ownersstore/LC_Page_Admin_OwnersStore_Ex.php
+++ data/class_extends/page_extends/admin/ownersstore/LC_Page_Admin_OwnersStore_Ex.php
@@ -21,7 +21,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-require_once CLASS_REALDIR . 'pages/admin/ownersstore/LC_Page_Admin_OwnersStore.php';
+require_once CLASS_REALDIR.'pages/admin/ownersstore/LC_Page_Admin_OwnersStore.php';
 
 /*
  * 決済モジュールやプラグイン、 user_data 以下の PHP で require されているため削除しないでください
